9b40820abb02755fd4aae61c564f3f0cc8b069b7
fredpena <f.ant.pena@gmail.com>
2025-07-13 10:29:03 -0400

    First commit

---
+++ src/main/java/dev/fredpena/dcm/DatabaseChangeManagementJavaApplication.java
@@ -0,0 +1,13 @@
+package dev.fredpena.dcm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabaseChangeManagementJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatabaseChangeManagementJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/fredpena/dcm/DatabaseChangeManagementJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.fredpena.dcm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabaseChangeManagementJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
